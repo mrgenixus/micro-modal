a562140e5674b24e325732b85653d5ead9ec7526
Benjamin West <mrgenixus@gmail.com>
2012-11-11 16:04:56 -0700

    added close-button section

--- micro-modal.js
+++ micro-modal.js
@@ -1,15 +1,26 @@
 $(function(){
 	//Create our overlay object
 	var overlay = $("<div id='modal-overlay'></div>");
-	var modalActivate = function(e) {
+	var setModalContent = function(content){
+		modalWindow.html(content);
+	}
+	
+	var modalActivate = function(e){
+		var modal_uri = $(this).data('load-uri'); 
+		if(modal_uri) resolve_content(modal_uri,function(data){
+			setModalContent(data);
+			_modalActivate.call(this,e);
+		});
+		else _modalActivate.call(this,e);
+	}
 
+	var _modalActivate = function(e) {
 		//Append the overlay to the document body
 		$("body").append(overlay.click(function() { 
 		  modalHide(); 
 		}))
 
-		var modal_uri = $(this).data('load-uri');
-		console.log(modal_uri);
+		
 		load_modal_content();
 
 		if (typeof document.body.style.maxHeight === "undefined") { //if IE 6
@@ -21,7 +32,13 @@ $(function(){
 		overlay.fadeIn(150);
 
 		//Prevent the anchor link from loading
-		e.preventDefault();
+		try {
+			e.preventDefault();
+			e.stopPropagation();	
+		} catch(err){
+			console.log(e,err,arguments);
+		}
+		
 
 		//Activate a listener 
 		$(document).keydown(handleEscape);		
@@ -51,8 +68,19 @@ $(function(){
 	var modalWindow = $('#modal-window')
 	modalWindow.on('contentChanged',modalActivate);
 
-	var load_modal_content = function (load_uri) {
-		if(load_uri != undefined) modalWindow.load(load_uri);
+	var resolve_content = function(load_uri,callback){
+		if($(load_uri).length > 0) callback($(load_uri).first().html());
+		else {
+			var uri = load_uri.split(' ').shift();
+			$.get(uri,function(data){
+				if(load_uri.split(' ').length > 1) data = data.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi,'');
+				var content = $('<div>').html(data).find(load_uri.split(' ').slice(1).join(' ')).html();
+				callback(content); 
+			});
+		}
+	}
+
+	var load_modal_content = function () {
 		if(modalWindow.length == 0) $('<div id="modal-window">').appendTo(document.body);
 		modalWindow.show();
 	    var windowWidth = modalWindow.width() / 2 ;
@@ -70,6 +98,7 @@ $(function(){
 		modalPanel: function() {
 			return this.each(function() {
 			  //Listen for clicks on objects passed to the plugin
+			  $($(this).data('load-uri')).hide();
 			  $(this).click(modalActivate).trigger('modal-initialized');
 			});
 		}
